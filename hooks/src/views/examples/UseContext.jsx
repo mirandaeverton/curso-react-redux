a6e2cf3acedd5014ff1b01772651f7167d4cb7c5
mirandaeverton <mir.everton@gmail.com>
2022-12-11 17:34:23 +0000

    Context API and useContext first exercise. Basic example showing the basics of Context API

--- hooks/src/views/examples/UseContext.jsx
+++ hooks/src/views/examples/UseContext.jsx
@@ -1,13 +1,35 @@
-import React from 'react'
+import React, { useContext } from 'react'
 import PageTitle from '../../components/layout/PageTitle'
 
+import DataContext from '../../Data/DataContext'
+
 const UseContext = (props) => {
+
+    const { state, setState } = useContext(DataContext)
+
+    function handleClick(delta) {
+        setState({
+            ...state,
+            number: state.number + delta
+        })
+    }
+
     return (
         <div className="UseContext">
             <PageTitle
                 title="Hook UseContext"
                 subtitle="Aceita um objeto de contexto e retorna o valor atual do contexto!"
             />
+            <div className="center">
+                <span className="text">{state.text}</span>
+                <span className="text">{state.number}</span>
+                <div>
+                    <button className="btn"
+                        onClick={() => handleClick(-1)} >-1</button>
+                    <button className="btn"
+                        onClick={() => handleClick(1)} >+1</button>
+                </div>
+            </div>
         </div>
     )
 }
